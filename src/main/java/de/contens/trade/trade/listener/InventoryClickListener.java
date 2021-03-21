c8569ad6ba062b7e78926e388c8591dd350e2ece
Contens <contensmc@gmail.com>
2021-03-21 19:21:49 +0100

    Add InventoryClickListener

---
+++ src/main/java/de/contens/trade/trade/listener/InventoryClickListener.java
@@ -0,0 +1,88 @@
+/*
+ * ******************************************************************************
+ *  * Copyright (C) 2021, Contens
+ *  * All rights reserved.
+ *  *
+ *  * Redistribution and use in source and binary forms, with or without
+ *  * modification, are permitted provided that the following conditions are met:
+ *  *
+ *  * 1. Redistributions of source code must retain the above copyright notice,
+ *  *    this list of conditions and the following disclaimer.
+ *  *
+ *  * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *  *    this list of conditions and the following disclaimer in the documentation
+ *  *    and/or other materials provided with the distribution.
+ *  *
+ *  * 3. Neither the name of the copyright holder nor the names of its contributors
+ *  *    may be used to endorse or promote products derived from this software
+ *  *    without specific prior written permission.
+ *  *
+ *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  * POSSIBILITY OF SUCH DAMAGE.
+ *  *****************************************************************************
+ */
+
+package de.contens.trade.trade.listener;
+
+import com.google.inject.Inject;
+import de.contens.trade.trade.TradeMap;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * @author Contens
+ * @created 21.03.2021
+ */
+
+public class InventoryClickListener implements Listener {
+
+    private TradeMap tradeMap;
+
+    @Inject
+    public InventoryClickListener(TradeMap tradeMap) {
+        this.tradeMap = tradeMap;
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
+
+        ItemStack itemStack = event.getCurrentItem();
+
+        player.sendMessage("Title: " + event.getView().getTitle());
+
+        /**
+         * Wieso, weshalb, warum entfernt Spigot den Support ab der 1.14 für Inventory#getName()
+         * -> Einen anderen Weg dafür finden. Vielleicht InventoryView#getTitle()?
+        if (event.getView().getTitle().equals("§6Handeln")) {
+            Trade trade = tradeMap.get(player.getName());
+
+            event.setCancelled(true);
+
+            if (event.getClickedInventory() != null) {
+                if (!event.getView().getTitle().equals("§6Handeln")) {
+                    trade.addItem(player, itemStack, event.getSlot());
+                } else {
+                    if (event.getSlot() == 47 || event.getSlot() == 51) {
+                        trade.handleConfirmation(player, event.getSlot());
+                    } else {
+                        trade.removeItem(player, event.getSlot());
+                    }
+                }
+            }
+        }
+         */
+    }
+}
